a9b5b1ed910091f00b25f726ea11d2bb24109c1d
mswift42 <martin.haesler@gmail.com>
2018-11-04 13:10:07 +0000

    Add Recipe struct.

--- ck.go
+++ ck.go
@@ -1,1 +1,27 @@
-package testhtml
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+)
+
+type Recipe struct {
+	title      string
+	subtitle   string
+	url        string
+	thumbnail  string
+	rating     string
+	difficulty string
+	preptime   string
+}
+
+func main() {
+	doc, err := goquery.NewDocument("https://www.chefkoch.de/rs/s0/bohnen/Rezepte.html#more2")
+	if err != nil {
+		fmt.Println(err)
+	}
+	doc.Find(".search-list-item-title").Each(func(i int, s *goquery.Selection) {
+		fmt.Println(s.Text())
+	})
+
+}
