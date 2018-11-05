1a46d5da50566202a555330b7fdc1ce6811ce983
mswift42 <martin.haesler@gmail.com>
2018-11-05 06:10:42 +0000

    var teststruct queryurls.

--- ck_test.go
+++ ck_test.go
@@ -2,9 +2,33 @@ package main
 
 import "testing"
 
+var queryurls = []struct {
+	st   string
+	page string
+	want string
+}{
+	{
+		"rotwein",
+		"0",
+		"https://www.chefkoch.de/rs/s0/rotwein/Rezepte.html#more2",
+	},
+	{
+		"rotwein",
+		"60",
+		"https://www.chefkoch.de/rs/s60/rotwein/Rezepte.html#more2",
+	},
+	{
+		"bohnen",
+		"120",
+		"https://www.chefkoch.de/rs/s120/bohnen/Rezepte.html#more2",
+	},
+}
+
 func TestQueryURL(t *testing.T) {
-	s1 := queryUrl("rotwein", "0")
-	if s1 != "https://www.chefkoch.de/rs/s0/rotwein/Rezepte.html#more2" {
-		t.Error("Expected s1 to be , got: ", s1)
+	for _, i := range queryurls {
+		qu := queryUrl(i.st, i.page)
+		if qu != i.want {
+			t.Errorf("Expected qu to be %s, got: %s", i.want, qu)
+		}
 	}
 }
