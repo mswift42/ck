8ded796ed41274e776e9603c942dc6231edeffed
mswift42 <martin.haesler@gmail.com>
2019-01-11 12:42:44 +0000

    test all fields in sahneRecipes.

--- ck_test.go
+++ ck_test.go
@@ -158,6 +158,45 @@ func TestNewRecipe(t *testing.T) {
 				results[ind].Difficulty)
 		}
 	}
+	file, err = ioutil.ReadFile("testhtml/sahne.html")
+	if err != nil {
+		t.Error(err)
+	}
+	doc, err = goquery.NewDocumentFromReader(bytes.NewReader(file))
+	if err != nil {
+		t.Error(err)
+	}
+	results = allRecipes(doc)
+	for ind, i := range sahneRecipes {
+		if results[ind].Title != i.title {
+			t.Errorf("Expected title to be %q, got: %q",
+				i.title, results[ind].Title)
+		}
+		if results[ind].Subtitle != i.subtitle {
+			t.Errorf("Expected subtitle to be %q, got: %q",
+				i.subtitle, results[ind].Subtitle)
+		}
+		if results[ind].Url != i.url {
+			t.Errorf("Expected url to be %q, got: %q",
+				i.url, results[ind].Url)
+		}
+		if results[ind].Thumbnail != i.thumbnail {
+			t.Errorf("Expected thumbnail to be %q, got: %q",
+				i.thumbnail, results[ind].Thumbnail)
+		}
+		if results[ind].Rating != i.rating {
+			t.Errorf("Expected rating to be %q, got: %q",
+				i.rating, results[ind].Rating)
+		}
+		if results[ind].Difficulty != i.difficulty {
+			t.Errorf("Expected difficulty to be %q, got: %q",
+				i.difficulty, results[ind].Difficulty)
+		}
+		if results[ind].Preptime != i.preptime {
+			t.Errorf("Expected preptime to be %q, got: %q",
+				i.preptime, results[ind].Preptime)
+		}
+	}
 }
 
 func TestRecipesToJSON(t *testing.T) {
